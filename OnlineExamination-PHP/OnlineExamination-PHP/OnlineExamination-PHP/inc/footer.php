434a25bddbbc0e5e76285015d72c9577d4b6c6b4
Rachana <152792371+Rachu9@users.noreply.github.com>
2024-08-14 14:10:16 +0530

    Add files via upload

---
+++ OnlineExamination-PHP/OnlineExamination-PHP/OnlineExamination-PHP/inc/footer.php
@@ -0,0 +1,24 @@
+</body>
+
+<!-- Footer -->
+	<section id="footer">
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-5">
+					
+				</div>
+			</div>	
+			<div class="row">
+				<div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center text-white">
+                    <p class="h6">© 2024 Developed by Likhith L ,Nayana S M and Rachana<br>Alvas Institute of Engineering and Technology Mijar,Moodbidri-574225</p>
+				</div>
+				</hr>
+			</div>	
+		</div>
+	</section>
+  <!-- ./Footer -->
+  
+
+<script src="vendor/jquery/jquery.min.js"></script>
+<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+</footer>
